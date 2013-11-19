83767a58ef1e028176ac8e3f085c2879899939ab
masatoshi.narita <masatoshi.narita@ever-rise.co.jp>
2013-11-19 11:25:59 +0900

    html_dir setting add.

--- attributes/default.rb
+++ attributes/default.rb
@@ -29,6 +29,7 @@ default['nginx']['dir']          = '/etc/nginx'
 default['nginx']['script_dir']   = '/usr/sbin'
 default['nginx']['log_dir']      = '/var/log/nginx'
 default['nginx']['binary']       = '/usr/sbin/nginx'
+default['nginx']['html_dir']     = '/var/www/nginx-default'
 
 case node['platform_family']
 when 'debian'
